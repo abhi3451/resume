735fe11cc8353c3d1342f401d673458cf978ab2b
abhi3451 <abhi3451@gmail.com>
2024-05-13 17:14:13 +0530

    new update

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,14 +21,14 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90  bg-gray-900`}
+        className={`${inter.className} text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90  bg-gray-200`}
       >
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             {children}
 
             <Toaster position="top-right" />
-            {/* <ThemeSwitch /> */}
+            <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
       </body>
