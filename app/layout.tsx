505609348799c75046090d3483378a118fbcc6f0
abhi3451 <abhi3451@gmail.com>
2024-05-20 11:08:57 +0530

    adding chat bot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import "./globals.css";
 import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
-
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,15 +20,45 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    console.log("Injecting chat support script");
+
+    (function (a: Document, b: string, c: any, d: string) {
+      console.log("Chat script initialization function called");
+
+      c[d] =
+        c[d] ||
+        function () {
+          (c[d].q = c[d].q || []).push(arguments);
+        };
+      c["_lsAlias"] = c[d];
+      const e = a.createElement(b) as HTMLScriptElement;
+      e.type = "text/javascript";
+      e.async = true;
+      e.src = "https://app.chatsupport.co/api/client/get/script/LS-e127443f";
+      const f = function () {
+        const g = a.getElementsByTagName(b)[0];
+        console.log("Inserting chat script into DOM");
+        g.parentNode?.insertBefore(e, g);
+      };
+      if (a.readyState === "complete") {
+        f();
+      } else {
+        c.addEventListener("load", f);
+      }
+    })(document, "script", window, "_ls");
+
+    (window as any)._ls("init", { projectId: "LS-e127443f" });
+  }, []);
+
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className}  bg-gray-200 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 `}
+        className={`${inter.className} bg-gray-200 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             {children}
-
             <Toaster position="top-right" />
             <ThemeSwitch />
           </ActiveSectionContextProvider>
--- components/loading.tsx
+++ components/loading.tsx
@@ -3,7 +3,7 @@ import "../app/globals.css";
 
 export const Loading = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center text-center m-auto">
       <button type="button" className="blinking" disabled>
         <p className="text-3xl">Wait for a moment...</p>
       </button>
